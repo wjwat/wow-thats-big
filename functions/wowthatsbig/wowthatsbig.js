eff7a01b664e4157a8b34b939eaa97548a7ab853
Will W <wjwat@onionslice.org>
2022-02-09 13:25:59 -0600

    Testing
    Co-authored-by: Ryan Bass <theryanbass@gmail.com>

--- functions/wowthatsbig/wowthatsbig.js
+++ functions/wowthatsbig/wowthatsbig.js
@@ -1,6 +1,8 @@
 const items = require('./wowthatsbig.json');
 
 exports.handler = async function(event, context) {
+  console.log(event);
+  console.log(context);
   return {
     statusCode: 200,
     body: JSON.stringify(items)
